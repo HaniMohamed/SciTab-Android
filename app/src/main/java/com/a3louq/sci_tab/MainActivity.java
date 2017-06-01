a6cbec0a5134aa1f849bdb8e19585ff78d410bce
HaniMohamed <hani.mohamed.hussein@gmail.com>
2017-06-01 15:07:56 +0000

    1

--- app/src/main/java/com/a3louq/sci_tab/MainActivity.java
+++ app/src/main/java/com/a3louq/sci_tab/MainActivity.java
@@ -2,6 +2,7 @@ package com.a3louq.sci_tab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"Test!! Submit on branch..",Toast.LENGTH_LONG).show();
+
     }
 }
