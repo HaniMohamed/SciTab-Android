3a2440ca159679af7d11691d9bc756cffbc104c6
HaniMohamed <hani.mohamed.hussein@gmail.com>
2017-06-01 00:09:35 +0000

    Initial

---
+++ app/src/main/java/com/a3louq/sci_tab/MainActivity.java
@@ -0,0 +1,13 @@
+package com.a3louq.sci_tab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
